c45c78140a6814a42232ab81e9bd5e8979790b56
visualstudio <visualstudio@122-PC31>
2019-02-01 22:06:29 -0500

    last

--- Ashwinder_week_04/Program.cs
+++ Ashwinder_week_04/Program.cs
@@ -10,6 +10,53 @@ namespace Ashwinder_week_04
     {
         static void Main(string[] args)
         {
+            Countryside c = new Countryside();
+            c.run();
+            c.Travel();
+            //new Test().forTest(); // Anonymous Object Reference
+            System.Console.ReadLine();
+        }
+    }
+
+    class Village
+    {
+        public bool isAstrilde = false;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+
+    class Countryside
+    {
+        Village Toronto, Ajax, Maple;
+
+        public Village CurrentVillage { get; private set; }
+
+        public void run()
+        {
+            Maple = new Village();
+            Maple.villageName = "Maple";
+            Toronto = new Village();
+            Toronto.villageName = "Toronto";
+            Ajax = new Village();
+            Ajax.isAstrilde = true;
+            Ajax.villageName = "Ajax";
+
+            Maple.nextVillage = Toronto;
+            Toronto.nextVillage = Ajax;
+            Ajax.nextVillage = null;
+        }
+
+        public void Travel()
+        {
+            CurrentVillage = Toronto;
+            while (true)
+            {
+                if (CurrentVillage.isAstrilde)
+                    System.Console.WriteLine("Astrilde is in : " + CurrentVillage.villageName);
+                else
+                    CurrentVillage = CurrentVillage.nextVillage;
+            }
         }
     }
 }
